34af89b4ff03fd7bce21ebe58c48f12428e83f1c
Ryan Koning <rkoning8@gmail.com>
2020-08-13 15:20:39 -0600

    Fixed menu > room > scene flow

---
+++ Assets/Scripts/Player/PlayerScore.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using Mirror;
+using System.Collections.Generic;
+
+/*
+	Documentation: https://mirror-networking.com/docs/Guides/NetworkBehaviour.html
+	API Reference: https://mirror-networking.com/docs/api/Mirror.NetworkBehaviour.html
+*/
+
+public class PlayerScore : NetworkBehaviour
+{
+
+    [SyncVar]
+    public int index;
+
+    [SyncVar]
+    public uint score;
+
+    /// <summary>
+    /// This is invoked for NetworkBehaviour objects when they become active on the server.
+    /// <para>This could be triggered by NetworkServer.Listen() for objects in the scene, or by NetworkServer.Spawn() for objects that are dynamically created.</para>
+    /// <para>This will be called for objects on a "host" as well as for object on a dedicated server.</para>
+    /// </summary>
+    public override void OnStartServer() {
+        index = connectionToClient.connectionId;
+    }
+
+    private void OnGUI() {
+        GUI.Box(new Rect(10f + (index * 110), 10f, 100f, 25f), $"P{index}: {score.ToString("0000000")}");
+    }
+}
--- Assets/Scripts/UI/RoomManager.cs
+++ Assets/Scripts/UI/RoomManager.cs
@@ -1,9 +1,168 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using Mirror;
 
+/*
+	Documentation: https://mirror-networking.com/docs/Components/NetworkRoomManager.html
+	API Reference: https://mirror-networking.com/docs/api/Mirror.NetworkRoomManager.html
+
+	See Also: NetworkManager
+	Documentation: https://mirror-networking.com/docs/Components/NetworkManager.html
+	API Reference: https://mirror-networking.com/docs/api/Mirror.NetworkManager.html
+*/
+
+/// <summary>
+/// This is a specialized NetworkManager that includes a networked room.
+/// The room has slots that track the joined players, and a maximum player count that is enforced.
+/// It requires that the NetworkRoomPlayer component be on the room player objects.
+/// NetworkRoomManager is derived from NetworkManager, and so it implements many of the virtual functions provided by the NetworkManager class.
+/// </summary>
 public class RoomManager : NetworkRoomManager
 {
-    
+
+    private bool showStartButton;
+
+    #region Server Callbacks
+
+    /// <summary>
+    /// This is called on the server when the server is started - including when a host is started.
+    /// </summary>
+    public override void OnRoomStartServer() { }
+
+    /// <summary>
+    /// This is called on the server when the server is stopped - including when a host is stopped.
+    /// </summary>
+    public override void OnRoomStopServer() { 
+        // Demonstrates how to get the Network Manager out of DontDestroyOnLoad when
+        // going to the offline scene to avoid collision with the one that lives there.
+        if (gameObject.scene.name == "DontDestroyOnLoad" && !string.IsNullOrEmpty(offlineScene) && SceneManager.GetActiveScene().path != offlineScene)
+            SceneManager.MoveGameObjectToScene(gameObject, SceneManager.GetActiveScene());
+
+        base.OnRoomStopServer();
+    }
+
+    /// <summary>
+    /// This is called on the host when a host is started.
+    /// </summary>
+    public override void OnRoomStartHost() { }
+
+    /// <summary>
+    /// This is called on the host when the host is stopped.
+    /// </summary>
+    public override void OnRoomStopHost() { }
+
+    /// <summary>
+    /// This is called on the server when a new client connects to the server.
+    /// </summary>
+    /// <param name="conn">The new connection.</param>
+    public override void OnRoomServerConnect(NetworkConnection conn) { }
+
+    /// <summary>
+    /// This is called on the server when a client disconnects.
+    /// </summary>
+    /// <param name="conn">The connection that disconnected.</param>
+    public override void OnRoomServerDisconnect(NetworkConnection conn) { }
+
+    /// <summary>
+    /// This is called on the server when a networked scene finishes loading.
+    /// </summary>
+    /// <param name="sceneName">Name of the new scene.</param>
+    public override void OnRoomServerSceneChanged(string sceneName) { }
+
+    /// <summary>
+    /// This allows customization of the creation of the room-player object on the server.
+    /// <para>By default the roomPlayerPrefab is used to create the room-player, but this function allows that behaviour to be customized.</para>
+    /// </summary>
+    /// <param name="conn">The connection the player object is for.</param>
+    /// <returns>The new room-player object.</returns>
+    public override GameObject OnRoomServerCreateRoomPlayer(NetworkConnection conn)
+    {
+        return base.OnRoomServerCreateRoomPlayer(conn);
+    }
+
+    /// <summary>
+    /// This allows customization of the creation of the GamePlayer object on the server.
+    /// <para>By default the gamePlayerPrefab is used to create the game-player, but this function allows that behaviour to be customized. The object returned from the function will be used to replace the room-player on the connection.</para>
+    /// </summary>
+    /// <param name="conn">The connection the player object is for.</param>
+    /// <param name="roomPlayer">The room player object for this connection.</param>
+    /// <returns>A new GamePlayer object.</returns>
+    public override GameObject OnRoomServerCreateGamePlayer(NetworkConnection conn, GameObject roomPlayer)
+    {
+        return base.OnRoomServerCreateGamePlayer(conn, roomPlayer);
+    }
+
+    /// <summary>
+    /// This allows customization of the creation of the GamePlayer object on the server.
+    /// <para>This is only called for subsequent GamePlay scenes after the first one.</para>
+    /// <para>See OnRoomServerCreateGamePlayer to customize the player object for the initial GamePlay scene.</para>
+    /// </summary>
+    /// <param name="conn">The connection the player object is for.</param>
+    public override void OnRoomServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnRoomServerAddPlayer(conn);
+    }
+
+    /// <summary>
+    /// This is called on the server when it is told that a client has finished switching from the room scene to a game player scene.
+    /// <para>When switching from the room, the room-player is replaced with a game-player object. This callback function gives an opportunity to apply state from the room-player to the game-player object.</para>
+    /// </summary>
+    /// <param name="conn">The connection of the player</param>
+    /// <param name="roomPlayer">The room player object.</param>
+    /// <param name="gamePlayer">The game player object.</param>
+    /// <returns>False to not allow this player to replace the room player.</returns>
+    public override bool OnRoomServerSceneLoadedForPlayer(NetworkConnection conn, GameObject roomPlayer, GameObject gamePlayer)
+    {
+        PlayerScore score = gamePlayer.GetComponent<PlayerScore>();
+        var player = roomPlayer.GetComponent<RoomPlayer>();
+        score.index = player.index;
+        return true;
+    }
+
+    /// <summary>
+    /// This is called on the server when all the players in the room are ready.
+    /// <para>The default implementation of this function uses ServerChangeScene() to switch to the game player scene. By implementing this callback you can customize what happens when all the players in the room are ready, such as adding a countdown or a confirmation for a group leader.</para>
+    /// </summary>
+    public override void OnRoomServerPlayersReady()
+    {
+        if (isHeadless) {
+            base.OnRoomServerPlayersReady();
+        } else {
+            showStartButton = true;
+        }
+    }
+
+    #endregion
+
+    #region Client Callbacks
+
+    /// <summary>
+    /// This is called on the client when the client stops.
+    /// </summary>
+    public override void OnRoomStopClient() { 
+        // Demonstrates how to get the Network Manager out of DontDestroyOnLoad when
+        // going to the offline scene to avoid collision with the one that lives there.
+        if (gameObject.scene.name == "DontDestroyOnLoad" && !string.IsNullOrEmpty(offlineScene) && SceneManager.GetActiveScene().path != offlineScene)
+            SceneManager.MoveGameObjectToScene(gameObject, SceneManager.GetActiveScene());
+
+        base.OnRoomStopClient();
+    }
+
+    #endregion
+
+    #region Optional UI
+
+    public override void OnGUI()
+    {
+        base.OnGUI();
+        if (allPlayersReady && showStartButton && GUI.Button(new Rect(150, 300, 120, 20), "START GAME"))
+        {
+            // set to false to hide it in the game scene
+            showStartButton = false;
+
+            ServerChangeScene(GameplayScene);
+        }
+    }
+
+    #endregion
 }
